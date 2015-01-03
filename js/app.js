bbb03cc196cf055baa8f3bd5ed84d1d8106f1b82
jukkamulla <jukkamulla@gmail.com>
2015-01-03 21:47:14 +0100

    add test fucntion for practice

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ if (debug===true)
 {
     console.log("This is my useful information.");
 }
+
+var yearlySalary = function(monthlySalary, bonus, workerType) {
+    if (workerType === "Manager")
+    {
+        bonus = bonus * 2;
+    }
+    var yearlySalary = monthlySalary * 12 + bonus;
+    console.log("The yearly salary: "+ yearlySalary);
+};
+yearlySalary(500, 37, "Manager");
