67a253f2cedee74ce079551d8fc727de4f126851
jukkamulla <jukkamulla@gmail.com>
2015-01-05 20:06:19 +0100

    using variables for practicing

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@ if (debug===true)
 {
     console.log("This is my useful information.");
 }
+var costOfBread = 5;
+var costOfButter = 3;
+var costOfMilk = 4;
+var tax = 1.27;
+var cost;
+var message_of_result = "Value of sumcost:";
 
-var yearlySalary = function(monthlySalary, bonus, workerType) {
-    if (workerType === "Manager")
-    {
-        bonus = bonus * 2;
-    }
-    var yearlySalary = monthlySalary * 12 + bonus;
-    console.log("The yearly salary: "+ yearlySalary);
-};
-yearlySalary(500, 37, "Manager");
+cost = costOfBread * tax;
+console.log(message_of_result + cost);
+cost += costOfButter * tax;
+console.log(message_of_result + cost);
+cost +=  costOfMilk * tax;
+console.log(message_of_result + cost);
