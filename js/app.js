ba905c5fcb0842bd9efa3b129950d3cfa5778d56
jukkamulla <jukkamulla@gmail.com>
2015-01-06 20:07:28 +0100

    using variables and function for practice

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
-console.log("test");
-var debug=true;
-if (debug===true)
-{
-    console.log("This is my useful information.");
-}
+var vatCalculator = function (netPrice, tax) {
+    var grossPrice = netPrice * tax;
+    console.log("The gross price is: " + grossPrice);
+};
+
 var costOfBread = 5;
 var costOfButter = 3;
 var costOfMilk = 4;
-var tax = 1.27;
-var cost;
-var message_of_result = "Value of sumcost:";
 
-cost = costOfBread * tax;
-console.log(message_of_result + cost);
-cost += costOfButter * tax;
-console.log(message_of_result + cost);
-cost +=  costOfMilk * tax;
-console.log(message_of_result + cost);
+var tax = 1.27;
+
+vatCalculator(costOfBread, tax);
+vatCalculator(costOfButter, tax);
+vatCalculator(costOfMilk, tax);
